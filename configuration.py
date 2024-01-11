5e2a9296d9bda9a6c53713f8d18da72a0f51c9bd
Руслан <r.shumigay@gmail.com>
2024-01-11 20:31:45 +0400

    Configured files

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://159f36dd-0d51-4cdd-84e5-35d2128e2741.serverhub.praktikum-services.ru"
+CREATE_PRODUCTS_KIT_PATH = "/api/v1/kits"
+CREATE_USER_PATH = "/api/v1/users/"
---
+++ create_kit_name_kit_test.py
@@ -0,0 +1,3 @@
+import data
+import sender_stand_request
+
